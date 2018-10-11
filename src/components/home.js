1c953f7504239afbd4cc4284711df6583ab57132
Sarah Han <sarah.s.han@gmail.com>
2018-10-11 16:11:57 -0700

    initial project setup completed

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">🔥 Chat 🔥</h1>
+        <h3 className="center">Come, Chat, Live</h3>
+    </div>
+);
