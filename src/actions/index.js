739e16715902ffc7a9c1b42a1eb8792b5857ecb8
Sarah Han <sarah.s.han@gmail.com>
2018-10-11 18:06:31 -0700

    Firebase chat in progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,16 @@ import types from './types';
 import { db } from '../firebase';
 
 export const getChatMessages = () => dispatch => {
-    db.ref('/').on('value', (snapshot) => {
+    const dbRef = db.ref('/chat-messages');
+
+    dbRef.on('value', (snapshot) => {
         console.log('DB Snapshot: ', snapshot.val());
+
+        dispatch({
+            type: types.GET_CHAT_MESSAGES,
+            messages: snapshot.val()
+        });
     });
+
+    return dbRef;
 }
--- src/components/chat.js
+++ src/components/chat.js
@@ -4,18 +4,44 @@ import { getChatMessages } from '../actions';
 
 class Chat extends Component {
     componentDidMount() {
-        this.props.getChatMessages();
+        this.dbRef = this.props.getChatMessages();
+
+        // console.log('DB Ref:', this.dbRef);
     }
 
+    componentWillUnmount() {
+        this.dbRef.off();
+    }
+    
     render() {
+        // console.log('Chat Props: ', this.props);
+        const { messages } = this.props;
+        const messageElements = Object.keys(messages).map(key => {
+            const { name, message } = messages[key];
+            return(
+                <li key={key} className="collection-item">
+                    <b>{name}: </b> {message}
+                </li>
+            );
+        });
+
         return(
             <div>
                 <h1 className="center">Chat Room</h1>
+                <ul className="collection">
+                    {messageElements}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, {
+function mapStateToProps (state) {
+    return {
+        messages: state.chat.messages
+    };
+}
+
+export default connect(mapStateToProps, {
     getChatMessages: getChatMessages
 })(Chat);
